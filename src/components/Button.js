3c73ef4abe26a7dc115b1cb08d2e4a80a3008fc5
koutilyanamdeo <70022358+koutilyanamdeo@users.noreply.github.com>
2021-11-08 18:05:46 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 const Button = (props) => {
-  const [disp, setdisp] = useState(false);
+//   const [disp, setdisp] = useState(false);
 
   function showPara() {
-    setdisp(true);
+    // setdisp(true);
 
-    if (setdisp) {
-      ReactDOM.render(
-        <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes
-          me so happy
-        </p>,
-        document.querySelector(".div-container")
-      );
-    }
+    ReactDOM.render(
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>,
+      document.querySelector(".div-container")
+    );
   }
   return (
     <div className="div-container">
