729bad6bc67ea7b128a152822fca8bbd01ebda19
koutilyanamdeo <70022358+koutilyanamdeo@users.noreply.github.com>
2021-11-08 17:58:24 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 const Button = (props) => {
-    const [disp, setdisp] = useState(false);
-  
-    function showPara() {
-      
-      setdisp(true);
-      
-      const para = document.querySelector("#para");
-      if(setdisp){
-        para.style.display = "block";
-      }
+  const [disp, setdisp] = useState(false);
+
+  function showPara() {
+    setdisp(true);
+
+    if (setdisp) {
+      ReactDOM.render(
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>,
+        document.querySelector(".div-container")
+      );
     }
-    return (
-      <div>
-        <p style={{display:"none"}} id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes me
-          so happy
-        </p>
-        <button onClick={showPara} id="click">Click for pargraph</button>
-      </div>
-    );
-  };
-  export default Button;
+  }
+  return (
+    <div className="div-container">
+      <button onClick={showPara} id="click">
+        Click for pargraph
+      </button>
+    </div>
+  );
+};
+export default Button;
